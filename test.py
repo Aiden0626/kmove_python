6c06a4c293d3b05e65b8c15447299986727edead
Aiden0626 <tjrans26@naver.com>
2019-07-15 17:45:59 +0900

    20190715  최종

---
+++ test.py
@@ -0,0 +1,26 @@
+print("hi")
+print("hi")
+food="python's favorite\n \"food\" is perl"
+print(food)
+print('123','한글','영어',sep='-',end='')
+print('싫어하는 윤곤')
+number1="윤곤이"
+number2="는"
+number3="바보다"
+number="석문이는"
+number33="똥멍청이다"
+print(number1+number2+number3)
+print(number+number33)
+print("==" * 50)
+print(len(food))
+number4="해인이는"
+print(number4+number33)
+print(food[-1])
+drink="abcdefghijk"
+print(drink[3:])
+a=3
+print('i eat %d apple.' %a)
+print('i love {1}you{0}'.format(1,2))
+name = 1
+age = 2
+f'나의 이름은 {name}이고 나이는{age}입니다'
